703af4c2d837d5dab4bd3341109b02469614500a
raje reddy <raje1reddy@gmail.com>
2019-08-17 14:23:12 +0100

    modified the local test variable

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,27 +2,28 @@ import java.util.Scanner;
 
 class Student {
     private double marks1, marks2, marks3;
-    //private double maxMarks = 100;
-    public static void main(String[] args) {
-            double maxMarks = 100;
-            System.out.println("Enter number of subjects");
-            Scanner sc = new Scanner(System.in);
-            double n = sc.nextDouble();
-            double[] marks = new double[3];
-            System.out.println("Enter marks");
-            for (int i = 0; i < n; i++) {
-                marks[i] = sc.nextDouble();
-            }
-            double avg = 0;
-            avg = ((marks[0] + marks[1] + marks[2]) / (maxMarks * 3)) * 100;
-            System.out.println(avg);
-
-        }
-
-
-
-    public void setAverage(double val) {
-        double avg = val;
+    private double maxMarks = 100;
+    public void setMarks1(double marks1) {
+        this.marks1 = marks1;
     }
+
+    public void setMarks2(double marks2) {
+        this.marks2 = marks2;
+    }
+
+    public void setMarks3(double marks3) {
+        this.marks3 = marks3;
+    }
+
+
+    public double getAverage() {
+        double avg = 0;
+        avg = ((marks1 + marks2 + marks3) / (maxMarks * 3)) * 100;
+        return avg;
+    }
+    //This code won't compile because avg is inaccessible outside te method  getAverage
+//    public void setAverage(double val) {
+//        avg = val;
+//    }
 
 }
